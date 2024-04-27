5be336a5bf2221ef6f0f5ddbb665e5724a32c462
Anton P <polevyy.anton@gmail.com>
2024-04-27 18:17:54 +1000

    Setup UI as micro frontend

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const viteConfig = defineViteConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'ui',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './UI': './src/components/UI',
+      },
+      shared: ['react'],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
