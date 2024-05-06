d96adc69b21239d809d6f1a9429d1cda8f7e04e5
Anton P <polevyy.anton@gmail.com>
2024-05-06 22:49:54 +1000

    Fixed: Use micro-app inside another micro-app by declaring remotes.d.ts

--- packages/container/src/pages/Auth.tsx
+++ packages/container/src/pages/Auth.tsx
@@ -1,19 +1,20 @@
 import { FC } from 'react';
-import Authentication, {
-  IAuthProps,
-  IRegistrationInput,
-  ILoginInput,
-} from 'auth/Authentication';
+import Authentication from 'auth/Authentication';
+// import Authentication, {
+//   IAuthProps,
+//   IRegistrationInput,
+//   ILoginInput,
+// } from 'auth/Authentication';
 import { AUTH } from '../constants/routes';
 
-const AuthPage: FC<IAuthProps> = () => {
-  const handleLoginSubmit = (data: ILoginInput) => {
-    console.log('containerapp : LoginForm Submited: ', data);
-  };
+const AuthPage: FC = () => {
+  // const handleLoginSubmit = (data: ILoginInput) => {
+  //   console.log('containerapp : LoginForm Submited: ', data);
+  // };
 
-  const handleRegistrationSubmit = (data: IRegistrationInput) => {
-    console.log('containerapp : SignUpForm Submited: ', data);
-  };
+  // const handleRegistrationSubmit = (data: IRegistrationInput) => {
+  //   console.log('containerapp : SignUpForm Submited: ', data);
+  // };
 
   const handleCancel = () => {
     console.log('container app: Login / Registration Canceled');
@@ -22,8 +23,10 @@ const AuthPage: FC<IAuthProps> = () => {
   return (
     <Authentication
       authPath={AUTH}
-      onLoginSubmit={handleLoginSubmit}
-      onRegistrationSubmit={handleRegistrationSubmit}
+      // onLoginSubmit={handleLoginSubmit}
+      // onRegistrationSubmit={handleRegistrationSubmit}
+      onLoginSubmit={() => console.log('onLoginSubmit')}
+      onRegistrationSubmit={() => console.log('onRegistrationSubmit')}
       onCancel={handleCancel}
     />
   );
---
+++ packages/container/src/remotes.d.ts
@@ -0,0 +1,4 @@
+declare module 'auth/*';
+
+// declare module 'auth/Authentication';
+// declare module 'auth/Authentication/IAuthProps';
