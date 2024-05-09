7ccfa96c45af8ff67fed65b2552153aeaf61c24c
Anton P <polevyy.anton@gmail.com>
2024-05-09 17:23:48 +1000

    Auth: Fixed routing bug

--- packages/auth/src/constants/routes.ts
+++ packages/auth/src/constants/routes.ts
@@ -1,6 +1,3 @@
-// export const AUTH_PATH = '/';
-// export const LOGIN_PATH = '/login';
-// export const SIGNUP_PATH = '/signup';
-export const ROOT_AUTH_PATH = '/auth/';
+// export const ROOT_AUTH_PATH = '/auth';
 export const LOGIN_PATH = '/login';
 export const SIGNUP_PATH = '/signup';
