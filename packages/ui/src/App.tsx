9db6b607f515e550f4afd9ea73c5830eddba9537
Anton P <polevyy.anton@gmail.com>
2024-05-09 16:52:44 +1000

    UI minor: App displays components

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,19 +1,48 @@
 import { useState } from 'react';
 
-import { Text, Button } from './components/UI';
+import { Text, Button, InputField } from './components/UI';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const ComponentWrapper = styled.div`
+  padding: 30px;
+`;
 
 function App() {
   const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState('');
 
   return (
-    <>
+    <Wrapper>
       <Text>UI Components</Text>
       <div>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+        <ComponentWrapper>
+          <Text>Button:</Text>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+        </ComponentWrapper>
+
+        <ComponentWrapper>
+          <Text>InputField: value = {inputValue}</Text>
+          <InputField
+            aria-label="username-input"
+            label="Username"
+            value={inputValue}
+            onChange={(newInput) => setInputValue(newInput)}
+          />
+        </ComponentWrapper>
       </div>
-    </>
+    </Wrapper>
   );
 }
 
